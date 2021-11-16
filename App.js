0d0d9cccbe302b749e70524b67d68ec39f308944
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-11-16 19:48:11 +0600

    Creating & Connecting Redux Store
    Module 4, Video : 1

--- App.js
+++ App.js
@@ -1,68 +1,16 @@
-import React, {useState} from 'react';
-import { StyleSheet, View } from 'react-native';
-import InputPlace from './components/InputPlace/InputPlace';
-import PlaceList from './components/PlaceList/PlaceList';
-import PlaceDetail from './components/PlaceDetail/PlaceDetail';
+import React from "react";
+import MainComponent from "./src/redux/MainComponent";
+import { Provider } from "react-redux";
+import Store from "./src/redux/store";
+
+
 
 export default function App() {
-  const [inputValue, setInputValue] = useState(""); 
-  const [palceList, setPalceList] = useState([]);
-  const [selectedPlace, setSelectedPlace] = useState(null);
-
-  const handleSelecetedPalce = key => {
-    const place = palceList.find(place => {
-      return place.key === key;
-    })
-    setSelectedPlace(place);
-  }
-
-  const handleHideModal = () => {
-    setSelectedPlace(null);
-  }
-
-  const handleDeleteItem = (key) => {
-    setPalceList(
-      palceList.filter(place => place.key !== key)
-    );
-    setSelectedPlace(null);
-
-  }
-
-  let PlaceDetails = null;
-  if (selectedPlace !== null) {
-    PlaceDetails = <PlaceDetail 
-      place={selectedPlace}
-      handleHideModal={handleHideModal}
-      handleDeleteItem={handleDeleteItem}
-    />
-  }
-
   return (
-    
-    <View style={styles.container}>
-      
-      {PlaceDetails}
-
-      <InputPlace 
-
-        inputValue={inputValue}
-        setInputValue={setInputValue}
-        palceList={palceList}
-        setPalceList={setPalceList}
-      
-      /> 
-       <PlaceList palceList={palceList} handleSelecetedPalce={handleSelecetedPalce} />
-    </View>
-  );
+    <Provider store={Store}>
+      <MainComponent />
+    </Provider>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    flexDirection: 'column',
-  },
-  
-});
+
---
+++ src/redux/MainComponent.js
@@ -0,0 +1,70 @@
+import React, {useState} from 'react';
+import { StyleSheet, View } from 'react-native';
+import InputPlace from './components/InputPlace/InputPlace';
+import PlaceList from './components/PlaceList/PlaceList';
+import PlaceDetail from './components/PlaceDetail/PlaceDetail';
+
+const MainComponent = (props) => {
+    const [inputValue, setInputValue] = useState(""); 
+    const [palceList, setPalceList] = useState([]);
+    const [selectedPlace, setSelectedPlace] = useState(null);
+  
+    const handleSelecetedPalce = key => {
+      const place = palceList.find(place => {
+        return place.key === key;
+      })
+      setSelectedPlace(place);
+    }
+  
+    const handleHideModal = () => {
+      setSelectedPlace(null);
+    }
+  
+    const handleDeleteItem = (key) => {
+      setPalceList(
+        palceList.filter(place => place.key !== key)
+      );
+      setSelectedPlace(null);
+  
+    }
+  
+    let PlaceDetails = null;
+    if (selectedPlace !== null) {
+      PlaceDetails = <PlaceDetail 
+        place={selectedPlace}
+        handleHideModal={handleHideModal}
+        handleDeleteItem={handleDeleteItem}
+      />
+    }
+  
+    return (
+      
+      <View style={styles.container}>
+        
+        {PlaceDetails}
+  
+        <InputPlace 
+  
+          inputValue={inputValue}
+          setInputValue={setInputValue}
+          palceList={palceList}
+          setPalceList={setPalceList}
+        
+        /> 
+         <PlaceList palceList={palceList} handleSelecetedPalce={handleSelecetedPalce} />
+      </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'flex-start',
+      flexDirection: 'column',
+    },
+    
+  });
+
+export default MainComponent;
