8e1f8b55aaa096d69d6018e9814c69c318ec757e
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-11-17 00:52:11 +0600

    Tab Navigation
    Video : 3

---
+++ src/components/FindPlaces/FindPlaces.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const FindPlaces = () => {
+    return (
+        <View>
+            <Text>Find Places</Text>
+        </View>
+    );
+};
+
+export default FindPlaces;
--- src/components/navigationTab/NavigationTab.js
+++ src/components/navigationTab/NavigationTab.js
@@ -1,11 +1,18 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
 import { Text, View } from 'react-native';
+import FindPlaces from '../FindPlaces/FindPlaces';
+import SharePlaces from '../SharePlaces/SharePlaces';
+
+
+const Tab = createBottomTabNavigator();
 
 const NavigationTab = (props) => {
     return (
-        <View>
-            <Text>Navigation Tab</Text>
-        </View>
+        <Tab.Navigator>
+            <Tab.Screen name="Share Places" component={SharePlaces} />
+            <Tab.Screen name="Find Places" component={FindPlaces} />
+        </Tab.Navigator>
     );
 };
 
