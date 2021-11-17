fd16ca58c6542cc5b8cf71592ff7fb7edc00cca6
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-11-18 00:15:31 +0600

    Using Google Firebase to Store Data
    Module : 09 , video : 1

--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -1,11 +1,15 @@
 import * as actionTypes from './actionType';
 
-export const addPlace = place => {
-    return {
-        type: actionTypes.ADD_PLACE,
-        payload: place
+export const addPlace = place => dispatch => {
+    fetch("https://my-places-d6079-default-rtdb.asia-southeast1.firebasedatabase.app/places.json", {
+        method: "POST",
+        body: JSON.stringify(place)
+    })
+        .catch(error => console.log(error))
+        .then(response => response.json())
+        .then(data => console.log(data))
     }
-}
+
 
 export const deletePlace = key => {
     return {
