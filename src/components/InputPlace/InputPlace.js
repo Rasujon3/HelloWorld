3ae80a59354b5925929d0ea6bf548cd6e8e2dc64
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-11-17 22:08:43 +0600

    building share places Components
    Video : 3

--- src/components/InputPlace/InputPlace.js
+++ src/components/InputPlace/InputPlace.js
@@ -6,33 +6,18 @@ const InputPlace = (props) => {
     return (
         <View style={styles.inputView}>
         <TextInput
-        style={{
-          width: "80%",
-          borderBottomWidth: 1,
-          borderColor: "green",
-          padding: 7,
-        }}
-        placeholder="Add a Place..."
+          style={{
+            width: "100%",
+            borderBottomWidth: 1,
+            borderColor: "green",
+            padding: 7,
+          }}
+        placeholder="Name of the Place..."
         value={props.inputValue}
         onChangeText={text => props.setInputValue(text)}
         />
-        <Button
-          title="Add"
-          onPress={()=> {
-            if (props.inputValue !== "") {
-            props.addPlace({
-              key: Math.random().toString(), 
-              value: props.inputValue,
-              image: {
-                uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Dhaka_14th_March_%2832624769393%29.jpg/1200px-Dhaka_14th_March_%2832624769393%29.jpg"
-              }
-            });
-            props.setInputValue("");
-            }
-          }}
-        >
-
-        </Button>
+        
+        
       </View>
     );
 };
--- src/components/SharePlaces/SharePlaces.js
+++ src/components/SharePlaces/SharePlaces.js
@@ -2,7 +2,8 @@ import React, {useState} from 'react';
 import InputPlace from '../InputPlace/InputPlace';
 import { addPlace } from '../../redux/actionCreators';
 import { connect } from 'react-redux';
-import { Text, View } from 'react-native';
+import {View,Button} from 'react-native';
+import PickImage from '../PickImage/PickImage';
 
 const mapDispatchToProps = dispatch => {
     return {
@@ -12,14 +13,36 @@ const mapDispatchToProps = dispatch => {
 
 const SharePlaces = (props) => {
     const [inputValue, setInputValue] = useState(""); 
+    const [image, setImage] = useState("");
     return (
+      <View>
+        <PickImage image={image} setImage={setImage} />
         <InputPlace 
-  
-        inputValue={inputValue}
-        setInputValue={setInputValue}
-        addPlace={props.addPlace}
-      
-      /> 
+          inputValue={inputValue}
+          setInputValue={setInputValue}
+        /> 
+        <View style={{
+          alignItems:"center"
+        }}>
+          <Button
+            title="Add"
+            onPress={()=> {
+              if (inputValue !== "") {
+              props.addPlace({
+                key: Math.random().toString(), 
+                value: inputValue,
+                image: {
+                  uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Dhaka_14th_March_%2832624769393%29.jpg/1200px-Dhaka_14th_March_%2832624769393%29.jpg"
+                }
+              });
+              setInputValue("");
+              }
+            }}
+          >
+          </Button>
+        </View>
+      </View>
+        
     );
 };
 
