b0ab3e5765abb978d33234addd7bb753b102fb67
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-11-17 12:10:00 +0600

    Spliting mainComponent
    Module : 7, Video: 1

--- src/components/FindPlaces/FindPlaces.js
+++ src/components/FindPlaces/FindPlaces.js
@@ -1,12 +1,73 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, View } from 'react-native';
+import PlaceList from '../PlaceList/PlaceList';
+import PlaceDetail from '../PlaceDetail/PlaceDetail';
+import { connect } from 'react-redux';
+import { deletePlace } from '../../redux/actionCreators';
 
-const FindPlaces = () => {
+
+const mapStateToProps = state => {
+  return {
+    palceList: state.palceList
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    deletePlace: key => dispatch(deletePlace(key)),
+  }
+}
+
+
+const FindPlaces = (props) => {
+    const [selectedPlace, setSelectedPlace] = useState(null);
+  
+    const handleSelecetedPalce = key => {
+      const place = props.palceList.find(place => {
+        return place.key === key;
+      })
+      setSelectedPlace(place);
+    }
+  
+    const handleHideModal = () => {
+      setSelectedPlace(null);
+    }
+  
+    const handleDeleteItem = (key) => {  
+      props.deletePlace(key);
+      setSelectedPlace(null);
+    }
+  
+    let PlaceDetails = null;
+    if (selectedPlace !== null) {
+      PlaceDetails = <PlaceDetail 
+        place={selectedPlace}
+        handleHideModal={handleHideModal}
+        handleDeleteItem={handleDeleteItem}
+      />
+    }
+  
     return (
-        <View>
-            <Text>Find Places</Text>
-        </View>
+      
+      <View style={styles.container}>
+        
+        {PlaceDetails}
+  
+
+         <PlaceList palceList={props.palceList} handleSelecetedPalce={handleSelecetedPalce} />
+      </View>
     );
 };
 
-export default FindPlaces;
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'flex-start',
+      flexDirection: 'column',
+    },
+    
+  });
+
+export default connect(mapStateToProps,mapDispatchToProps)(FindPlaces);
--- src/components/SharePlaces/SharePlaces.js
+++ src/components/SharePlaces/SharePlaces.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
+import InputPlace from '../InputPlace/InputPlace';
+import { addPlace } from '../../redux/actionCreators';
+import { connect } from 'react-redux';
 import { Text, View } from 'react-native';
 
-const SharePlaces = () => {
+const mapDispatchToProps = dispatch => {
+    return {
+      addPlace: place => dispatch(addPlace(place)),
+    }
+  }
+
+const SharePlaces = (props) => {
+    const [inputValue, setInputValue] = useState(""); 
     return (
-        <View>
-            <Text>Share Places</Text>
-        </View>
+        <InputPlace 
+  
+        inputValue={inputValue}
+        setInputValue={setInputValue}
+        addPlace={props.addPlace}
+      
+      /> 
     );
 };
 
-export default SharePlaces;
+export default connect(null, mapDispatchToProps)(SharePlaces);
