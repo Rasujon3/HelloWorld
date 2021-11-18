c95207feda68a030d233ba9188165960f91dcc53
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-11-18 11:29:24 +0600

    Styling input fields
    Module : 10, Video: 1

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,17 +1,72 @@
-import React from 'react';
-import { Button, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Button, Text, TextInput, View,StyleSheet, TouchableOpacity, Touchable } from 'react-native';
 
 const Login = (props) => {
+    const [authState, setAuthState] = useState({
+        mode: "login",
+        input: {
+            email: "",
+            password: "",
+            confirmPassword: "",
+        }
+    })
     return (
-        <View>
-            <Text>Login</Text>
-            <Button title="Login" onPress={
-                ()=> {
-                    props.navigation.navigate('Home')
-                }
-            }></Button>
+        <View style={styles.loginView}>
+            <TextInput 
+                style={styles.input}
+                placeholder="Your Email Address"
+                value={authState.input.email}
+            />
+            <TextInput 
+                style={styles.input}
+                placeholder="Password"
+                value={authState.input.password}
+
+            />
+            <TextInput 
+                style={styles.input}
+                placeholder="Confirm Password"
+                value={authState.input.confirmPassword}
+
+            />
+            <TouchableOpacity style={styles.btnContainer}>
+                <Text style={styles.btnStyle}>{authState.mode==="login" ? "Login" : "Sign Up"}</Text>
+            </TouchableOpacity>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    loginView: {
+        flex: 1,
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    input: {
+        width: "85%",
+        padding: 5,
+        marginTop: 10,
+        backgroundColor: "#eee",
+        borderWidth: 1,
+        borderColor: "#009688",
+        borderRadius: 4
+    },
+    btnStyle: {
+        fontSize: 16,
+        color: "#fff",
+        alignSelf: "center"
+    },
+    btnContainer: {
+        flexDirection: "row",
+        width: 150,
+        paddingVertical: 5,
+        backgroundColor: "#009688",
+        borderRadius: 5,
+        marginTop: 10,
+        justifyContent: "center",
+        alignItems: "center"
+    }
+})
 
 export default Login;
